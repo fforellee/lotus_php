202cb4c08b0e9c05340b35fbcad3444dc6e90d4f
fforelle <xdollboyx@gmail.com>
2021-11-17 00:34:39 -0300

    Alterado

--- estrutura-site/php/login.php
+++ estrutura-site/php/login.php
@@ -1,15 +1,18 @@
 <?php 
+	session_start();
 	if(!isset($_SESSION['login'])) {
+
 		if(isset($_POST['acao'])) {
 			$log = 'saul@123';
 			$sen = '123';
 			$senFor = $_POST["senhaLogin"];
 			$logFor = $_POST['emailLogin'];				
+
 			if($log == $logFor && $sen == $senFor){
 				$_SESSION['login'] = true;
-				session_start();
 				header('Location: ../html/horti.php');
 			}			
+
 		}
 	}else{
 		if(isset($_GET['logout'])){
